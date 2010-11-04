0df3a83ee6421bb840d090f054cb15e2dc1339ac
Stephen Jayna <ext-stephen.jayna@nokia.com>
2010-11-04 17:13:10 +0000

    Result of running "perltidy -i=2 -l=175 Extension.pm".

--- Extension.pm
+++ Extension.pm
@@ -17,7 +17,8 @@
 # Initial Developer. All Rights Reserved.
 #
 # Contributor(s):
-#   YOUR NAME <YOUR EMAIL ADDRESS>
+#   Jari Savolainen <ext-jari.a.savolainen@nokia.com>
+#   Stephen Jayna <ext-stephen.jayna@nokia.com>
 
 package Bugzilla::Extension::Dashboard;
 use strict;
@@ -40,139 +41,157 @@ use File::Path;
 
 our $VERSION = '0.01';
 
-# See the documentation of Bugzilla::Hook ("perldoc Bugzilla::Hook" 
+# See the documentation of Bugzilla::Hook ("perldoc Bugzilla::Hook"
 # in the bugzilla directory) for a list of all available hooks.
 sub install_update_db {
-    my ($self, $args) = @_;
+  my ( $self, $args ) = @_;
 }
 
 # Hook for page.cgi and dashboard
 sub page_before_template {
-  my ($self, $args) = @_;
-  my ($vars, $page) = @$args{qw(vars page_id)};
+  my ( $self, $args ) = @_;
+  my ( $vars, $page ) = @$args{qw(vars page_id)};
 
-  if ($page =~ /^dashboard(_ajax)?\.(html|js)/ ) {
+  if ( $page =~ /^dashboard(_ajax)?\.(html|js)/ ) {
     $vars->{debug_info} = "Hooked page";
-    
+
     my $user_id = Bugzilla->user->id;
-    
-    my $datadir = bz_locations()->{'datadir'};
-    my $dataextdir = $datadir . '/extensions/Dashboard';
+
+    my $datadir     = bz_locations()->{'datadir'};
+    my $dataextdir  = $datadir . '/extensions/Dashboard';
     my $datauserdir = $dataextdir . '/' . $user_id;
-    
-    if ($user_id>0) {
-      
+
+    if ( $user_id > 0 ) {
       my $cgi = Bugzilla->cgi;
-      
+
       $vars->{debug_info} .= " | user_id: " . $user_id;
       $vars->{debug_info} .= " | datauserdir: " . $datauserdir;
-      
-      if (-d $datauserdir) {
-        ## old user, load prefs folder
-        $vars->{debug_info} .= '| old user';
 
-      } else {
-        ## new user, create prefs folder
+      if ( -d $datauserdir ) {
+
+        # old user, load prefs folder
+        $vars->{debug_info} .= '| old user';
+      }
+      else {
+
+        # new user, create prefs folder
         $vars->{debug_info} .= '| new user';
-        
+
         mkpath( $datauserdir, { verbose => 0, mode => 0755, error => \my $err } );
 
         if (@$err) {
           for my $diag (@$err) {
             my ( $file, $message ) = each %$diag;
             print "Problem making $file: $message\n";
-          }                                          
-          die("Couldn't create $datauserdir");       
+          }
+          die("Couldn't create $datauserdir");
         }
       }
-      
+
       $vars->{cgi_variables} = { Bugzilla->cgi->Vars };
-      if ($page eq "dashboard_ajax.html") {
+
+      if ( $page eq "dashboard_ajax.html" ) {
         $vars->{debug_info} .= " | widget";
-        if (Bugzilla->cgi->param('action') eq 'load') {
+
+        if ( Bugzilla->cgi->param('action') eq 'load' ) {
           $vars->{debug_info} .= " | load";
-          
+
           $vars->{debug_info} .= " | " . Bugzilla->cgi->param('widget_id');
-          my $widget_id = int($cgi->param('widget_id'));
-          if ($widget_id>0 && -e $datauserdir . "/" . $widget_id . ".widget") {
+          my $widget_id = int( $cgi->param('widget_id') );
+
+          if ( $widget_id > 0 && -e $datauserdir . "/" . $widget_id . ".widget" ) {
             my $widget = retrieve( $datauserdir . "/" . $widget_id . ".widget" );
-            $vars->{widget}=$widget;
-          } else {
+            $vars->{widget} = $widget;
+          }
+          else {
             $vars->{debug_info} .= " | illegal widget id";
           }
-        } elsif (Bugzilla->cgi->param('action') eq 'delete') {
+        }
+        elsif ( Bugzilla->cgi->param('action') eq 'delete' ) {
           $vars->{debug_info} .= " | delete";
-          
+
           $vars->{debug_info} .= " | " . Bugzilla->cgi->param('widget_id');
-          my $widget_id = int($cgi->param('widget_id'));
-          
-          if ($widget_id>0 && -e $datauserdir . "/" . $widget_id . ".widget") {
-            my @files = glob $datauserdir . "/" . $widget_id .".*";
+          my $widget_id = int( $cgi->param('widget_id') );
+
+          if ( $widget_id > 0 && -e $datauserdir . "/" . $widget_id . ".widget" ) {
+            my @files = glob $datauserdir . "/" . $widget_id . ".*";
+
             foreach my $dir_entry (@files) {
               trick_taint($dir_entry);
               unlink $dir_entry;
-              $vars->{debug_info} .= " | ".$dir_entry." deleted";
+              $vars->{debug_info} .= " | " . $dir_entry . " deleted";
             }
-          } else {
+          }
+          else {
             $vars->{debug_info} .= " | illegal widget id";
           }
-        } elsif (Bugzilla->cgi->param('action') eq 'save') {
+        }
+        elsif ( Bugzilla->cgi->param('action') eq 'save' ) {
           $vars->{debug_info} .= " | save";
-          
+
           $vars->{debug_info} .= " | " . Bugzilla->cgi->param('widget_id');
-          my $widget_id = int($cgi->param('widget_id'));
-          if ($widget_id>0)
-          {
+          my $widget_id = int( $cgi->param('widget_id') );
+
+          if ( $widget_id > 0 ) {
             trick_taint($widget_id);
-            
+
             my (%widget);
-            
-            #numerical fields
+
+            # numerical fields
             my @fields = qw(id pos col height);
+
             foreach (@fields) {
-              %widget->{$_} = int($cgi->param("widget_".$_));
+              %widget->{$_} = int( $cgi->param( "widget_" . $_ ) );
             }
-            #true/false fields
+
+            # true/false fields
             my @fields = qw(movable removable collapsible editable resizable resized maximizable minimized);
+
             foreach (@fields) {
-              %widget->{$_} = $cgi->param("widget_".$_) eq 'true' ? 1 : 0;
+              %widget->{$_} = $cgi->param( "widget_" . $_ ) eq 'true' ? 1 : 0;
             }
-            #text fields
-            my @fields = qw(title);
+
+            # text fields
+            my @fields   = qw(title);
             my $scrubber = HTML::Scrubber->new;
             $scrubber->default(0);
+
             foreach (@fields) {
-              %widget->{$_} = $scrubber->scrub($cgi->param("widget_".$_));
+              %widget->{$_} = $scrubber->scrub( $cgi->param( "widget_" . $_ ) );
             }
-            
-            #color
+
+            # color
             %widget->{'color'} = $cgi->param("widget_color") =~ /color-(gray|yellow|red|blue|white|orange|green)/ ? $1 : "gray";
 
-            foreach my $key (sort keys %widget) {
-              $vars->{debug_info} .= ", $key=".%widget->{$key};
+            foreach my $key ( sort keys %widget ) {
+              $vars->{debug_info} .= ", $key=" . %widget->{$key};
             }
-                   
+
             store \%widget, $datauserdir . "/" . $widget_id . ".widget";
-          } else {
+          }
+          else {
             $vars->{debug_info} .= " | illegal widget id";
           }
         }
-      } else {
-        opendir(DIR, $datauserdir) or die($!);
-        my (@files,$file);
-        @files = grep(/\.widget$/,readdir(DIR));
+      }
+      else {
+        opendir( DIR, $datauserdir ) or die($!);
+        my ( @files, $file );
+        @files = grep( /\.widget$/, readdir(DIR) );
         closedir(DIR);
+
         foreach $file (@files) {
           $vars->{debug_info} .= " | " . $file;
-          my $widget = retrieve($datauserdir . "/" . $file);
-          $vars->{"column".$widget->{col}}->[$widget->{pos}] = $widget;
+          my $widget = retrieve( $datauserdir . "/" . $file );
+          $vars->{ "column" . $widget->{col} }->[ $widget->{pos} ] = $widget;
         }
+
         $vars->{debug_info} .= " | default";
       }
-      
-    } else {
-      ThrowUserError('login_required'); 
-    }        
+    }
+    else {
+      ThrowUserError('login_required');
+    }
   }
 }
 
