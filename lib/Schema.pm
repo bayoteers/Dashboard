3f45ff79ff658248da31ef2f2a404fc8d0552a0d
David Wilson <dw@botanicus.net>
2011-12-13 11:16:49 +0200

    Split schema checking out into Schema.pm.

---
+++ lib/Schema.pm
@@ -0,0 +1,203 @@
+# -*- Mode: perl; indent-tabs-mode: nil -*-
+#
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Original Code is the Unified Dashboard Bugzilla Extension.
+#
+# The Initial Developer of the Original Code is "Nokia Corporation"
+# Portions created by the Initial Developer are Copyright (C) 2010 the
+# Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   David Wilson <ext-david.3.wilson@nokia.com>
+#
+
+package Bugzilla::Extension::Dashboard::Schema;
+
+use strict;
+use Exporter 'import';
+use Bugzilla::Extension::Dashboard::Config;
+use Bugzilla::Util;
+use HTML::Scrubber;
+
+our @EXPORT = qw(
+    COLUMN_DEFS
+    OVERLAY_DEFS
+    parse
+    to_bool
+    to_color
+    to_int
+    to_text
+    WIDGET_DEFS
+);
+
+use constant WIDGET_DEFS => {
+    # Background colour.
+    color => { type => 'color', required => 1, default => 'gray' },
+    # Integer 0..cols widget column number.
+    col => { type => 'int', required => 1 },
+    # Widget height in pixels.
+    height => { type => 'int' },
+    # Widget ID
+    id => { type => 'int', required => 1, min => 1 },
+    # Bool 0..1, widget content is hidden.
+    minimized => { type => 'bool' },
+    # Password for RSS widget.
+    password => { type => 'text', private => 1 },
+    # Integer 0..rows widget position in column.
+    pos => { type => 'int', required => 1 },
+    # Refresh interval in seconds.
+    refresh => { type => 'int', default => 600, required => 1 },
+    # CSS selector for URL widget.
+    selector => { type => 'text' },
+    # Widget title.
+    title => { type => 'text', required => 1 },
+    # Contents of 'text' widget.
+    text => { type => 'text' },
+    # Type, one of 'url', 'rss', 'mybugs', 'xeyes'.
+    type => { type => 'text', required => 1, choices => [ WIDGET_TYPES ]},
+    # URL for 'url' widget.
+    URL => { type => 'text' },
+    # Username for 'rss' widget.
+    username => { type => 'text', private => 1 }
+};
+
+use constant COLUMN_DEFS => {
+    # Width in percent.
+    width => { type => 'int', required => 1 }
+};
+
+use constant OVERLAY_DEFS => {
+    # List of columns.
+    columns => { type => 'list', item => COLUMN_DEFS, required => 1 },
+    # Seconds since epoch creation time.
+    created => { type => 'int', required => 1 },
+    # Long description.
+    description => { type => 'text', required => 1, default => '' },
+    # Seconds since epoch last save time.
+    modified => { type => 'int', required => 1 },
+    # Short name.
+    name => { type => 'text', required => 1, min_length => 4 },
+    # Owner user ID.
+    owner => { type => 'int', required => 1 },
+    # Shared overlay hasn't been approved yet?
+    pending => { type => 'int', default => 0 },
+    # Shared between all users?
+    shared => { type => 'bool', default => 0, required => 1 },
+    # List of widgets.
+    widgets => { type => 'list', item => WIDGET_DEFS },
+    # Bool, overlay represents a user's workspace.
+    workspace => { type => 'bool', default => 0 },
+};
+
+use constant TYPE_CONVERTER_MAP => {
+    int => \&to_int,
+    bool => \&to_bool,
+    text => \&to_text,
+    color => \&to_color,
+};
+
+
+sub _parse_field {
+    my ($field, $def, $value) = @_;
+
+    if(defined($def->{min}) && $value < $def->{min}) {
+        my $min = $def->{min};
+        die "Field '$field' must be at least $min.";
+    } elsif(defined($def->{min_length}) && length($value) < $def->{min_length}) {
+        my $min = $def->{min_length};
+        die "Field '$field' must be at least $min long.";
+    } elsif(defined($def->{default}) && !defined($value)) {
+        $value = $def->{default};
+    } elsif($def->{required} && !defined($value)) {
+        die 'Missing required field: ' . $field;
+    } elsif($def->{choices} && defined($value)
+            && !grep($_ eq $value, @{$def->{choices}})) {
+        my $choices = join(', ', @{$def->{choices}});
+        die "Field $field invalid value '$value'; ".
+            "must be one of $choices";
+    }
+
+    my $converter = TYPE_CONVERTER_MAP->{$def->{type}};
+    return &$converter($value);
+}
+
+sub _parse_list {
+    my ($field, $def, $value) = @_;
+
+    if($def->{required} && !defined($value)) {
+        die "Field '$field' is required and must be an array.";
+    } elsif(! defined($value)) {
+        return [];
+    } elsif(! UNIVERSAL::isa($value, 'ARRAY')) {
+        die "Field '$field' must be an array.";
+    }
+
+    return [ map { parse($def->{item}, $_) } @$value ];
+}
+
+sub parse {
+    my ($defs, $fields, $check_required) = @_;
+
+    my $out = {};
+    while(my ($field, $def) = each(%$defs)) {
+        my $value = $fields->{$field};
+
+        if($def->{type} eq 'list') {
+            $out->{$field} = _parse_list($field, $def, $value);
+        } else {
+            $out->{$field} = _parse_field($field, $def, $value);
+        }
+    }
+
+    return $out;
+}
+
+
+#
+# Type conversion functions.
+#
+
+sub to_int {
+    my ($s) = @_;
+    detaint_signed($s);
+    return int($s || 0);
+}
+
+sub to_bool {
+    my ($s) = @_;
+    $s ||= '';
+    return int($s eq 'true' || ($s =~ /\d+/ && $s == 1));
+}
+
+sub to_text {
+    my ($s, $default) = @_;
+    if ($s) {
+        my $scrubber = HTML::Scrubber->new;
+        $scrubber->default(0);
+        return trim($scrubber->scrub($s));
+    }
+    return trim($default || '');
+}
+
+sub to_color {
+    my ($s) = @_;
+    $s ||= '';
+
+    if($s =~ /^(?:color-)?(gray|yellow|red|blue|white|orange|green)$/) {
+        return $1;
+    }
+
+    return 'gray';
+}
+
+
+1;
--- lib/Util.pm
+++ lib/Util.pm
@@ -20,64 +20,6 @@
 #   David Wilson <ext-david.3.wilson@nokia.com>
 #   Jari Savolainen <ext-jari.a.savolainen@nokia.com>
 #
-# General notes:
-#
-#   * Data is kept in perl Storable format files under EXTENSION_DIR
-#     (nominally, bugzila_root/data/extensions/Dashboard).
-#
-#   * EXTENSION_DIR contains a subdirectory for each user, with the directory
-#     name being the user's integer ID. The subdirectory with the ID "0" is
-#     used for storing shared overlay files.
-#
-#   * Within each user directory, there is one 'preferences' file, zero or more
-#     '*.widget' files, and possibly one 'overlay' file, as documented below.
-#
-#
-# Preferences file keys:
-#
-#       columns:
-#           Integer columns configured (default: COLUMNS_DEFAULT).
-#       column(0..columns):
-#           Integer column width in percent.
-#
-#
-# Widget file keys:
-#
-#       id:
-#           Integer widget ID
-#       col:
-#           Integer 0..cols widget column number.
-#       pos:
-#           Integer 0..rows widget position in column.
-#       height:
-#           Integer widget height in pixels.
-#       type:
-#           String widget type, one of 'url', 'rss', 'mybugs', 'xeyes'.
-#       resized:
-#           Bool 0..1, force widget resize on next reload.
-#       minimized:
-#           Bool 0..1, widget content is hidden.
-#       refresh:
-#           Integer, unknown.
-#
-#
-# Overlay file keys:
-#
-#       shared:
-#           Bool 0..1, widget is shared between all users.
-#       name:
-#           String overlay short name.
-#       description:
-#           String overlay description.
-#       owner:
-#           Integer owner user ID.
-#       created:
-#           Integer seconds since UNIX epoch creation time.
-#       username:
-#           String username for RSS/mybugs widget, empty for shared.
-#       password:
-#           String password for RSS/mybugs widget, empty for shared.
-#
 
 package Bugzilla::Extension::Dashboard::Util;
 
@@ -85,89 +27,35 @@ use strict;
 use warnings;
 
 use Exporter 'import';
-our @EXPORT_OK = qw(
-    cgi_no_cache
-    clear_user_workspace
-    dir_glob
+our @EXPORT = qw(
+    first_free_id
+    get_mtime
     get_overlay_dir
-    get_shared_overlay_dir
-    WIDGET_FIELD_DEFS
-    COLUMN_FIELD_DEFS
-    OVERLAY_FIELD_DEFS
-    get_user_dir
-    get_user_overlay_dir
-    get_user_overlays
-    get_user_prefs
+    get_overlays_dir
+    overlays_for_user
     merge
-    get_user_widget
-    get_user_widgets
-    fields_from_params
-    fixup_types
-    validate_fields
-    get_widget_path
-    is_valid_widget_type
-    load_user_overlay
-    make_path
-    scrub_string
-    set_user_prefs
-    set_user_widgets
-    to_bool
-    to_color
-    to_int
+    migrate_workspace
 );
 
 use Data::Dumper;
-use List::Util qw(sum);
-
 use File::Basename;
-use File::Copy;
-use File::Path;
+use File::Copy qw(move);
+use File::Path qw(mkpath remove_tree);
 use File::Spec;
-use POSIX qw(floor strftime isdigit);
+use List::Util qw(sum);
+use POSIX qw(strftime);
 use Storable qw(store retrieve);
-use HTML::Scrubber;
 
 use Bugzilla::Constants;
 use Bugzilla::Extension::Dashboard::Config;
+use Bugzilla::Extension::Dashboard::Schema;
 use Bugzilla::User;
 use Bugzilla::Util;
 
 
-use constant WIDGET_FIELD_DEFS => {
-    color => { type => 'color', required => 1, default => 'gray' },
-    col => { type => 'int', required => 1 },
-    height => { type => 'int' },
-    id => { type => 'int', required => 1, min => 1 },
-    minimized => { type => 'bool' },
-    password => { type => 'text' },
-    pos => { type => 'int', required => 1 },
-    refresh => { type => 'int', default => 600, required => 1 },
-    selector => { type => 'text' },
-    title => { type => 'text', required => 1 },
-    text => { type => 'text' },
-    type => { type => 'text', required => 1, choices => [ WIDGET_TYPES ]},
-    URL => { type => 'text' },
-    username => { type => 'text' },
-    width => { type => 'int' },
-};
-
-use constant OVERLAY_FIELD_DEFS => {
-    description => { type => 'text', required => 1, default => '' },
-    name => { type => 'text', required => 1, min_length => 4 },
-    shared => { type => 'bool', default => 0, required => 1 },
-};
-
-use constant COLUMN_FIELD_DEFS => {
-    width => { type => 'int', required => 1 }
-};
-
-use constant TYPE_CONVERTER_MAP => {
-    int => \&to_int,
-    bool => \&to_bool,
-    text => \&scrub_string,
-    color => \&to_color
-};
-
+#
+# Data helper functions.
+#
 
 # Given a list of hashrefs, return a new hashref which is the result of
 # assigning the elements from each hash in order to an empty hash. Skips list
@@ -187,76 +75,18 @@ sub merge {
 };
 
 
-sub fields_from_params {
-    my ($defs, $params) = @_;
-    my $fields;
-
-    while(my ($field, $value) = each(%$params)) {
-        my $def = $defs->{$field};
-        if(defined($def)) {
-            my $converter = TYPE_CONVERTER_MAP->{$def->{type}};
-            $fields->{$field} = &$converter($value);
-        }
-    }
-
-    return $fields;
-}
+#
+# Filesystem helper functions.
+#
 
 
-sub validate_fields {
-    my ($defs, $fields, $check_required) = @_;
-
-    while(my ($field, $def) = each(%$defs)) {
-        my $value = $fields->{$field};
-
-        if(defined($def->{min}) && $value < $def->{min}) {
-            my $min = $def->{min};
-            die "Field '$field' must be at least $min.";
-        } elsif(defined($def->{min_length}) && length($value) < $def->{min_length}) {
-            my $min = $def->{min_length};
-            die "Field '$field' must be at least $min long.";
-        } elsif(defined($def->{default}) && !defined($value)) {
-            $fields->{$field} = $def->{default};
-        } elsif($def->{required} && $check_required && !defined($value)) {
-            die 'Missing required field: ' . $field;
-        } elsif($def->{choices} && defined($value)
-                && !grep($_ eq $value, @{$def->{choices}})) {
-            my $choices = join(', ', @{$def->{choices}});
-            die "Field $field invalid value '$value'; ".
-                "must be one of $choices";
-        }
-    }
-}
-
-
-sub fixup_types {
-    my ($defs, $fields) = @_;
-    while(my ($field, $def) = each(%$defs)) {
-        my $converter = TYPE_CONVERTER_MAP->{$def->{type}};
-        my $value = $fields->{$field};
-        $fields->{$field} = &$converter($value);
-    }
-}
-
-sub cgi_no_cache {
-    my $headers = {
-        -expires       => 'Sat, 26 Jul 1997 05:00:00 GMT',
-        -Last_Modified => strftime('%a, %d %b %Y %H:%M:%S GMT', gmtime),
-        -Pragma        => 'no-cache',
-        -Cache_Control => join(
-            ', ', qw(
-              private no-cache no-store must-revalidate max-age=0
-              pre-check=0 post-check=0)
-        )
-    };
-
-    foreach my $key (%$headers) {
-        print Bugzilla->cgi->header($key, $headers->{$key});
-    }
-}
-
+# Create a directory if it doesn't already exist.
 sub make_path {
     my ($path) = @_;
+
+    if(-d $path) {
+        return;
+    }
 
     mkpath($path, {
         verbose => 0,
@@ -273,6 +103,8 @@ sub make_path {
     }
 }
 
+
+# Glob some pattern and detaint each returned result.
 sub dir_glob {
     my @out;
     foreach my $path (glob File::Spec->catfile(@_)) {
@@ -282,109 +114,232 @@ sub dir_glob {
     return @out;
 }
 
-sub get_extension_dir {
-    return File::Spec->catdir(bz_locations()->{'datadir'}, EXTENSION_DIR);
+
+# Return the modification time of a path in seconds since epoch.
+sub get_mtime {
+    my ($path) = @_;
+    my @st = lstat $path;
+    return @st[9];
 }
 
+
+# Find an unused overlay ID we can use to store the loaded page's unsaved
+# changes to. When the user explicitly saves the workspace, this overlay is
+# destroyed.
+sub first_free_id {
+    my ($dir) = @_;
+    my $dest_dir;
+    my $id = 0;
+
+    do {
+        $dest_dir = File::Spec->catdir($dir, ++$id);
+    } until (!-d $dest_dir);
+
+    return int($id);
+}
+
+
+# Return a user's data directory.
 sub get_user_dir {
     my ($user_id) = @_;
     $user_id = Bugzilla->user->id if !defined($user_id);
     trick_taint $user_id;
-    return File::Spec->catdir(get_extension_dir(), $user_id);
+
+    my $ext_dir = File::Spec->catdir(bz_locations()->{datadir},
+        'extensions/Dashboard');
+    return File::Spec->catdir($ext_dir, $user_id);
 }
 
-sub get_user_overlay_dir {
+
+# Return the directory containing the overlays for the given user.
+sub get_overlays_dir {
     my ($user_id) = @_;
     return File::Spec->catdir(get_user_dir($user_id), 'overlay');
 }
 
+
+# Return the directory containing a particular overlay.
 sub get_overlay_dir {
     my ($user_id, $overlay_id) = @_;
-    return File::Spec->catdir(get_user_overlay_dir($user_id), $overlay_id);
+    return File::Spec->catdir(get_overlays_dir($user_id), $overlay_id);
 }
 
-sub get_overlay {
-    my ($user_id, $overlay_id) = @_;
-    if(!defined $user_id) {
-        $user_id = Bugzilla->user->id;
+
+#
+# Widget IO functions.
+#
+
+
+# Write a list of widgets to the given directory, then delete any other widget
+# files from the directory that weren't in the list.
+sub widgets_to_dir {
+    my ($dir, $widgets) = @_;
+
+    my $paths;
+    foreach my $widget (@{$widgets}) {
+        my $path = File::Spec->catfile($dir, $widget->{id} . '.widget');
+        store $widget, $path;
+        $paths->{$path} = 1;
     }
 
-    my $dir = get_overlay_dir($user_id, $overlay_id);
-    my $active  = File::Spec->catfile($dir, 'overlay');
-    my $pending = File::Spec->catfile($dir, 'overlay.pending');
-
-    foreach my $path (($active, $pending)) {
-        if (-f $path) {
-            my $overlay = retrieve($path);
-            $overlay->{"id"} = int(basename dirname $path);
-            $overlay->{"user_id"}    = $user_id;
-            $overlay->{"user_login"} = user_id_to_login($overlay->{"owner"});
-            $overlay->{"pending"}    = int($path eq $pending);
-            return $overlay;
-        }
-    }
+    my @old = dir_glob($dir, '*.widget');
+    unlink grep { !$paths->{$_} } @old;
 }
 
-sub get_user_overlays {
+
+# Return widgets from the given directory as an arrayref.
+sub widgets_from_dir {
+    my ($dir) = @_;
+    my @widgets = map { retrieve $_; } dir_glob($dir, '*.widget');
+    return [ map { parse(WIDGET_DEFS, $_) } @widgets ];
+}
+
+
+# Given an array ref of widget hashrefs, remove any private fields present.
+sub blank_private_fields {
+    my ($defs, $widgets) = @_;
+
+    my @private;
+    while(my ($field, $def) = each(%$defs)) {
+        push @private, $field if $def->{private};
+    }
+
+    map { delete @$_->{@private} } @$widgets;
+}
+
+
+#
+# Overlay IO functions.
+#
+
+
+# Read an overlay from a directory.
+sub overlay_from_dir {
+    my ($user_id, $dir) = @_;
+
+    my $path = File::Spec->catfile($dir, 'overlay');
+    if(! -f $path) {
+        die "Cannot load, overlay file missing.";
+    }
+    my $overlay = retrieve($path);
+
+    $path = File::Spec->catfile($dir, 'preferences');
+    if(-f $path) {
+        $overlay = merge($overlay, retrieve($path));
+    }
+
+    normalize_columns($overlay);
+    $overlay->{widgets} = widgets_from_dir($dir);
+
+    if($user_id != $overlay->{user_id}) {
+        blank_private_fields(WIDGET_DEFS, $overlay->{widgets});
+    }
+
+    return $overlay;
+};
+
+
+# Write an overlay to a directory.
+sub overlay_to_dir {
+    my ($dir, $overlay) = @_;
+
+    my $tmp = $dir . '.tmp';
+    if(-d $tmp) {
+        remove_tree $tmp;
+    }
+    make_path $tmp;
+
+    normalize_columns($overlay);
+    $overlay = parse(OVERLAY_DEFS, $overlay);
+    $overlay->{modified} = time;
+
+    my $path = File::Spec->catfile($tmp, 'overlay');
+    if($overlay->{pending}) {
+        $path .= '.pending';
+    }
+
+    # Widgets are stored separately for now; copy $prefs to preserve the hash
+    # for any callers.
+    widgets_to_dir($tmp, $overlay->{widgets});
+    my $copy = merge($overlay);
+    delete $copy->{widgets};
+    store $copy, $path;
+
+    # Everything written, now drop any old dir and replace with the new one.
+    if(-d $dir) {
+        remove_tree $dir;
+    }
+    move $tmp, $dir;
+};
+
+
+# Return a list of all overlays for a user.
+sub overlays_for_user {
     my ($user_id) = @_;
-    my $overlay_dir = get_user_overlay_dir($user_id);
+    $user_id = Bugzilla->user->id if !defined($user_id);
 
     my @overlays;
-    foreach my $dir (dir_glob($overlay_dir, '*')) {
-        my $overlay = get_overlay($user_id, basename $dir);
-        if($overlay) {
-            push @overlays, $overlay;
+    foreach my $dir (dir_glob(get_overlays_dir($user_id), '*')) {
+        my $active  = File::Spec->catfile($dir, 'overlay');
+        my $prefs = File::Spec->catfile($dir, 'preferences');
+        my $pending = File::Spec->catfile($dir, 'overlay.pending');
+
+        my $base = {};
+        if(-f $prefs) {
+            $base = retrieve($prefs);
+        }
+
+        foreach my $path (($active, $pending)) {
+            next if !-f $path;
+            my $overlay = merge($base, retrieve($path));
+            $overlay->{id} = int(basename dirname $path);
+            $overlay->{user_id}    = $user_id;
+            $overlay->{user_login} = user_id_to_login($overlay->{owner});
+            $overlay->{pending}    = int($path eq $pending);
+            if(! $overlay->{modified}) {
+                $overlay->{modified} = get_mtime $path;
+            }
+            normalize_columns($overlay);
+            push @overlays, parse(OVERLAY_DEFS, $overlay);
         }
     }
 
     return @overlays;
 }
 
-sub get_shared_overlay_dir {
-    return get_user_overlay_dir(0);
-}
 
-sub get_user_pref_path {
+# Migrate an old style user workspace to a private overlay, if necessary.
+sub migrate_workspace {
     my ($user_id) = @_;
-    return File::Spec->catfile(get_user_dir($user_id), 'preferences');
-}
+    $user_id = Bugzilla->user->id if !defined($user_id);
 
-# create default preferences
-sub make_empty_prefs {
-    my $prefs = {
-        widgets => [],
-        columns => []
-    };
-
-    foreach(1..COLUMNS_DEFAULT) {
-        my $column = { width => int(100 / COLUMNS_DEFAULT) };
-        push @{$prefs->{columns}}, $column;
+    my $user_dir = get_user_dir($user_id);
+    my $prefs_path = File::Spec->catfile($user_dir, 'preferences');
+    if(! -f $prefs_path) {
+        return;
     }
 
-    return $prefs;
+    my $mtime = get_mtime $prefs_path;
+    my $time_str = strftime('%a, %Y-%m-%d %H:%M:%S', gmtime $mtime);
+
+    my $overlay = parse(OVERLAY_DEFS, merge(retrieve($prefs_path), {
+        created => $mtime,
+        modified => $mtime,
+        description => 'Last active workspace from old Dashboard',
+        name => 'Workspace from ' . $time_str,
+        owner => $user_id,
+        widgets => widgets_from_dir($user_dir),
+        workspace => 1,
+    }));
+
+    my $overlay_dir = get_overlay_dir($user_id, $mtime);
+    overlay_to_dir $overlay_dir, $overlay;
+    unlink $prefs_path, grep { -f $_ } $user_dir;
 }
 
-sub get_user_prefs {
-    my ($user_id) = @_;
-    my $path = get_user_pref_path($user_id);
-    my $prefs;
 
-    if (-f $path) {
-        $prefs = retrieve($path);
-        $prefs->{widgets} = [ get_user_widgets($user_id) ];
-    } else {
-        $prefs = make_empty_prefs();
-    }
-
-    normalize_columns($prefs);
-    return $prefs;
-}
-
-# The new-style column structure is an array named "columns", with each element
-# a hashref with a single key, "width". The old structure was the keys
-# column0..columnN, with the width as their value, along with a "columns"
-# integer, which was never set correctly. If the old structure is detected
-# here, convert it to the new structure.
+# Convert old Dashboard's list of column keys to a new style columns structure,
+# if necessary. Then normalize the column widths.
 sub normalize_columns {
     my ($prefs) = @_;
 
@@ -394,134 +349,19 @@ sub normalize_columns {
         map { delete $prefs->{$_} } @keys;
     }
 
+    if(! @{$prefs->{columns}}) {
+        $prefs->{columns} = [
+            { width => 33 },
+            { width => 33 },
+            { width => 33 }
+        ];
+    }
+
     # If column totals don't add up to 100%, spread the difference out.
     my $total = sum map { $_->{width} } @{$prefs->{columns}};
     my $delta = int((100 - $total) / @{$prefs->{columns}});
     map { $_->{width} += $delta } @{$prefs->{columns}};
 }
 
-
-sub set_user_prefs {
-    my ($user_id, $prefs) = @_;
-    my $path = get_user_pref_path($user_id);
-
-    # new user, create prefs folder
-    if (!-d dirname $path) {
-        make_path dirname $path;
-    }
-
-    normalize_columns($prefs);
-    set_user_widgets($user_id, $prefs->{widgets});
-
-    # Widgets are stored separately for now; copy $prefs to preserve the hash
-    # for any callers.
-    my $to_save = merge($prefs);
-    delete $to_save->{widgets};
-    store $to_save, $path;
-}
-
-sub get_user_widgets {
-    my ($user_id) = @_;
-    my @paths = dir_glob(get_user_dir($user_id), '*.widget');
-    my @widgets = map { retrieve $_; } @paths;
-    map { fixup_types WIDGET_FIELD_DEFS, $_ } @widgets;
-    return @widgets;
-}
-
-sub set_user_widgets {
-    my ($user_id, $widgets) = @_;
-
-    my $paths;
-    foreach my $widget (@{$widgets}) {
-        my $path = get_widget_path($user_id, $widget->{id});
-        store $widget, $path;
-        $paths->{$path} = 1;
-    }
-
-    my @old = dir_glob(get_user_dir($user_id), "*.widget");
-    unlink grep { !$paths->{$_} } @old;
-}
-
-sub get_widget_path {
-    my ($user_id, $widget_id) = @_;
-    my $user_dir = get_user_dir($user_id);
-    return File::Spec->catfile($user_dir, $widget_id . '.widget');
-}
-
-sub get_user_widget {
-    my ($user_id, $widget_id) = @_;
-    my $path = get_widget_path($user_id, $widget_id);
-    if (!-f $path) {
-        return undef;
-    }
-
-    my $widget = retrieve($path);
-    fixup_types WIDGET_FIELD_DEFS, $widget;
-    return $widget;
-}
-
-sub to_bool {
-    my ($s) = @_;
-    $s ||= '';
-    return int($s eq 'true' || ($s =~ /\d+/ && $s == 1));
-}
-
-sub to_color {
-    my ($s) = @_;
-    $s ||= '';
-
-    if($s =~ /^(?:color-)?(gray|yellow|red|blue|white|orange|green)$/) {
-        return $1;
-    }
-
-    return 'gray';
-    #ThrowUserError('dashboard_invalid_color');
-}
-
-sub to_int {
-    my ($s) = @_;
-    detaint_signed($s);
-    return int($s || 0);
-}
-
-sub is_valid_widget_type {
-    my ($s) = @_;
-    $s ||= '';
-    return grep($_ eq $s, WIDGET_TYPES) ? 1 : 0;
-}
-
-sub scrub_string {
-    my ($s, $default) = @_;
-    if ($s) {
-        my $scrubber = HTML::Scrubber->new;
-        $scrubber->default(0);
-        return trim($scrubber->scrub($s));
-    }
-    return trim($default || '');
-}
-
-
-# Clear the user's workspace and load a new overlay.
-sub clear_user_workspace {
-    my ($user_id) = @_;
-    set_user_prefs($user_id, make_empty_prefs());
-}
-
-
-# Clear the user's workspace and load a new overlay.
-sub load_user_overlay {
-    my ($user_id, $overlay_user_id, $overlay_id) = @_;
-    clear_user_workspace $user_id;
-
-    my $source_dir = get_overlay_dir($overlay_user_id, $overlay_id);
-    my $user_dir = get_user_dir($user_id);
-
-    foreach my $path (dir_glob($source_dir, '*')) {
-        if(-f $path && basename($path) !~ /^overlay/) {
-            copy($path, $user_dir)
-              or die "Copy failed: $!";
-        }
-    }
-}
 
 1;
