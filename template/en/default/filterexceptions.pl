331565fee415d2da997e2f5705d301ef87cbc775
Pami Ketolainen <pami.ketolainen@jollamobile.com>
2013-10-29 21:10:14 +0200

    Make Bugzilla tests pass

---
+++ template/en/default/filterexceptions.pl
@@ -0,0 +1,13 @@
+# -*- Mode: perl; indent-tabs-mode: nil -*-
+
+%::safe = (
+
+'hook/global/user-error-errors.html.tmpl' => [
+  'id',
+],
+
+'pages/dashboard.html.tmpl' => [
+  "dashboard_config || 'null'",
+],
+
+);
